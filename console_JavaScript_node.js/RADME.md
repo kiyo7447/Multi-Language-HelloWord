fa0f75125343611a1ff31ffa4a665390e43f074e
Kiyotaka Abe <kiyo7447@gmaill.com>
2023-09-17 23:46:34 +0900

    add Console JavaScript

---
+++ console_JavaScript_node.js/RADME.md
@@ -0,0 +1,11 @@
+﻿# 実行方法
+```bash
+node hello.js
+```
+
+# Docker
+```bash
+docker build -t hello-node .
+
+docker run --name hello-node --rm hello-node
+```
---
+++ console_JavaScript_node.js/hello-world.js
@@ -0,0 +1,16 @@
+﻿// コンソールから入力を受け取る
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// ユーザーに質問を投げる
+rl.question('あなたの名前は何ですか？ ', (name) => {
+  // 入力された名前を出力する
+  console.log(`こんにちは、${name}さん！`);
+
+  // 入力をクローズする
+  rl.close();
+});
